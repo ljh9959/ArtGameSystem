098f21fd99bb116110f3968051ee50cf958b7c74
ljh9959 <ljhun8433@gmail.com>
2019-11-18 15:44:18 +0900

    Copy <Animations - sprite sheet>
    Just copy the example

--- sketch.js
+++ sketch.js
@@ -1,14 +1,27 @@
-let canvas;
-let canvasWidth = 600;
-let canvasHeight = 400;
+//Creating animations from sprite sheets
+var sprite_sheet_image;
+var sprite_sheet;
+var explode_animation;
+
+function preload() {
+  // specify width and height of each frame and number of frames
+  sprite_sheet = loadSpriteSheet('assets/explode_sprite_sheet.png', 171, 158, 11);
+  explode_animation = loadAnimation(sprite_sheet);
+
+  // load the full sprite sheet for example reference only
+  sprite_sheet_image = loadImage('assets/explode_sprite_sheet.png');
+}
 
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-  noCursor();
+  createCanvas(800, 225);
 }
 
 function draw() {
-  background(30);
-  ellipse(mouseX, mouseY, 100, 100);
+  clear();
+
+  // animate the sprite sheet
+  animation(explode_animation, 100, 130);
+
+  // show full sheet for example reference
+  image(sprite_sheet_image, 250, 40, 500, 154);
 }
