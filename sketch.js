81fbc1709f35a633fbb7c6294948d06ae1c80d06
ljh9959 <ljhun8433@gmail.com>
2019-12-17 10:20:32 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 let canvas;
 let canvasWidth = 800;
 let canvasHeight = 400;
@@ -72,19 +71,11 @@ function setup() {
   bgm.loop();
   bgmOnOff = 1;
   zoomzoom = 0.4;
-=======
-//Sprite creation
-//Click to create a new sprite with random speed
-
-function setup() {
-  createCanvas(800, 400);
->>>>>>> Stashed changes
 }
 
 function draw() {
   background(255, 255, 255);
 
-<<<<<<< Updated upstream
 
   //mouse trailer, the speed is inversely proportional to the mouse distance
   ghost.velocity.x = (camera.mouseX - ghost.position.x) / 20;
@@ -166,25 +157,4 @@ function draw() {
   //the absolute 0,0 (try to see what happens if you don't turn it off
   camera.off();
   image(frame, 0, 0);
-=======
-  fill(0);
-  textAlign(CENTER);
-  text('Click to create a new sprite', width/2, height/2);
-  //draw all the sprites added to the sketch so far
-  //the positions will be updated automatically at every cycle
-  drawSprites();
-}
-
-function mousePressed() {
-
-  //create a sprite at the mouse position and store it in a temporary variable
-  var s = createSprite(mouseX, mouseY, 30, 30);
-  //if no image or animation is associated it will be a rectancle of the specified size
-  //and a random color
-
-  //now you can use the variable to set properties
-  //e.g. a random velocity on the x and y coordinates
-  s.velocity.x = random(-5, 5);
-  s.velocity.y = random(-5, 5);
->>>>>>> Stashed changes
 }
